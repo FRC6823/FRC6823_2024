fb36593ecea96ddf93e1ebfcf08ebc4063ff1f94
ryanmbruce <ryanmbruce@gmail.com>
2024-02-16 19:01:52 -0600

    Revert "created a limit switch for the arm motors"
    This reverts commit 2b23b30feabd5b89b8f2d6ec74462a881deacf7f.

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -10,7 +10,6 @@ import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.SparkLimitSwitch;
 
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -27,7 +26,6 @@ public class ArmSubsystem extends SubsystemBase{
     private SparkLimitSwitch rev_LimitSwitch5, rev_LimitSwitch6;
     private double setPoint;
     private double setPoint2;
-    private DigitalInput limitSwitch = new DigitalInput(1);
     
     public ArmSubsystem () {
         motor5 = new CANSparkMax(11, MotorType.kBrushless);
@@ -90,22 +88,15 @@ public class ArmSubsystem extends SubsystemBase{
     }
 
     public void set(double speed){
-        if (speed > 0) {
-            if (limitSwitch.get()) {
-                motor5.set(0);
-                motor6.set(0);
-            } else {
-                motor5.set(speed);
-                motor6.set(speed);
-            }
-        }
-
+            motor5.set(speed);
+            motor6.set(speed);
     }
     public double getEncoderPosition() {
         return encoder.getPosition();
     }
     @Override
-    public void periodic() {
+    public void periodic()
+    {
         SmartDashboard.putNumber("Arm Encoder", getEncoderPosition());
         // SmartDashboard.putNumber("Speed", ((speed + 1) /2));
 
