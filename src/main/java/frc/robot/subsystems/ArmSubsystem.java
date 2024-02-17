0252d457f17838fcb4767d1ce954626575cd2d7b
6823Code <usmrobotics@ga.usmk12.org>
2024-02-17 11:13:39 -0600

    Updated arm control

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,17 +1,12 @@
 package frc.robot.subsystems;
 
-import java.util.concurrent.CancellationException;
-
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.SparkAbsoluteEncoder;
 import com.revrobotics.SparkAbsoluteEncoder;
 import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.SparkLimitSwitch;
 
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.Const;
@@ -27,16 +22,28 @@ public class ArmSubsystem extends SubsystemBase{
     private SparkLimitSwitch rev_LimitSwitch5, rev_LimitSwitch6;
     private double setPoint;
     private double setPoint2;
-    private DigitalInput limitSwitch = new DigitalInput(1);
+    private double armSpeed;
     
     public ArmSubsystem () {
+        
+        /*
+         * To Do: Change all CAN IDs to come from Const.java
+         */
         motor5 = new CANSparkMax(11, MotorType.kBrushless);
         motor6 = new CANSparkMax(15, MotorType.kBrushless);
 
-        /* Make sure you are configuring the Sparks in CODE not in the firmaware */
+        /*
+         * Make sure you are configuring the Sparks in CODE not in firmware (via usb)
+         * so that if you have to replace a Spark quickly,
+         * you don't have to fight with any config except the CAN ID
+        */
         motor5.restoreFactoryDefaults();
         motor6.restoreFactoryDefaults();
-
+        motor6.follow(motor5, true);
+        /*
+         * Define the switches.
+         * LimitSwitches are enabled upon creation, but we have the option to disable here
+         */
         fwd_LimitSwitch5 = motor5.getForwardLimitSwitch(SparkLimitSwitch.Type.kNormallyClosed);
         rev_LimitSwitch5 = motor5.getReverseLimitSwitch(SparkLimitSwitch.Type.kNormallyClosed);
         fwd_LimitSwitch5.enableLimitSwitch(false);
@@ -47,11 +54,32 @@ public class ArmSubsystem extends SubsystemBase{
         fwd_LimitSwitch6.enableLimitSwitch(false);
         rev_LimitSwitch6.enableLimitSwitch(true);
         
+        /*
+         * Resist arm movement when at "rest"
+         */
         motor5.setIdleMode(IdleMode.kBrake);
         motor6.setIdleMode(IdleMode.kBrake);
-        motor6.setInverted(true);
+        
+        /* 
+         * Motor 6 faces the opposite direction of motor 5, so invert it
+         * Doing this allows us to send the same speed to both and get the right motion
+         * 
+         * To Do:
+         *      -Rename these so they use robot left or robot right
+         *      -Configure one motor to follow the other (follower mode)
+         *          does this work with absolute encoders and limit switches?
+        */
+        //motor6.setInverted(true);
+
+        //  Write the config to flash memory on the Spark Max so that the settings can survive a brownout/power outage.
+        motor5.burnFlash();
+        motor6.burnFlash();
+
         pidController = motor5.getPIDController();
         pidController2 = motor6.getPIDController();
+        armSpeed = 0;
+        setPoint = 0;
+        setPoint2 = 0;
 
         encoder = motor6.getAbsoluteEncoder(SparkAbsoluteEncoder.Type.kDutyCycle);
 
@@ -82,32 +110,37 @@ public class ArmSubsystem extends SubsystemBase{
         pidController2.setSmartMotionAllowedClosedLoopError(Const.Arm.allowedErr2, smartMotionSlot2);
     }
 
-    public void goToAngle(double setPoint, double setPoint2)
-    {
+    public void goToAngle(double setPoint){
         this.setPoint = setPoint;
-        motor5.set(setPoint);
-        motor6.set(setPoint2);
+        //motor5.set(setPoint);
+        //motor6.set(setPoint2);
+        pidController.setReference(setPoint, CANSparkMax.ControlType.kPosition);
     }
 
     public void set(double speed){
-        if (speed > 0) {
-            if (limitSwitch.get()) {
-                motor5.set(0);
-                motor6.set(0);
-            } else {
-                motor5.set(speed);
-                motor6.set(speed);
-            }
-        }
-
+        /*
+         * This limits the totla speed rate.  Should NOT do it this way!
+         * To Do: May want to implement a Slew Rate Limiter for arm, put a global arm speed constant in dashboard?
+         */
+        armSpeed = speed * .2;
+        SmartDashboard.putNumber("Arm Speed", speed);
+        motor5.set(speed);
+            //motor6.set(speed);
     }
+    
     public double getEncoderPosition() {
         return encoder.getPosition();
     }
+
     @Override
-    public void periodic() {
+    public void periodic(){
         SmartDashboard.putNumber("Arm Encoder", getEncoderPosition());
-        // SmartDashboard.putNumber("Speed", ((speed + 1) /2));
+        SmartDashboard.putBoolean("Fwd 5 & 6 Limit Enabled", fwd_LimitSwitch5.isLimitSwitchEnabled() && fwd_LimitSwitch6.isLimitSwitchEnabled() );
+        SmartDashboard.putBoolean("Rev 5 & 6 Limit Enabled", rev_LimitSwitch5.isLimitSwitchEnabled() && rev_LimitSwitch6.isLimitSwitchEnabled() );
+        SmartDashboard.putBoolean("Forward Limit Status", fwd_LimitSwitch5.isPressed() && fwd_LimitSwitch6.isPressed());
+        SmartDashboard.putBoolean("Reverse Limit Status", rev_LimitSwitch5.isPressed() && rev_LimitSwitch6.isPressed());
+        
+        // SmartDashboard.putNumber("Speed", speed);
 
         //motor5.set(setPoint);
         //motor6.set(setPoint2);
